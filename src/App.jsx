434c77244407fa6cbc1732ae21463b1b505726c1
scriptkid-ke <mwangialex813@gmail.com>
2025-07-22 11:48:12 +0300

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
@@ -18,13 +18,13 @@ const App = () => {
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Navbar />
       <Routes>
-        <Route path ="/" element={<Home/>}/>
-        <Route path = "/collection" element = {<Collection/>} />
-        <Route path = "/about" element ={<About/>} />
-        <Route path = "/contact" element = {<Contact/>} />
-        <Route path = "/product/:productId" element = {<Product/>}/>
-        <Route path = "/cart"  element = {<Cart/>}/>
-        <Route path = "/login" element= {<Login/>}/>
+        <Route path ="/" element={<Home/>} />
+        <Route path = "/collection" element={<Collection/>} />
+        <Route path = "/about" element={<About/>} />
+        <Route path = "/contact" element={<Contact/>} />
+        <Route path = "/product/:productId" element={<Product/>}/>
+        <Route path = "/cart"  element={<Cart/>}/>
+        <Route path = "/login" element={<Login/>}/>
         <Route path = "/place-order" element={<PlaceOrder/>} />
         <Route path = "/orders" element={<Orders/>} />
       </Routes>
